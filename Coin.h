5173cac5a85411ab98706ce56bf1ae2233f2a013
= <sim.sheline@gmail.com>
2019-11-25 19:41:27 +0800

    coin disappears when collided with

--- Coin.h
+++ Coin.h
@@ -3,11 +3,12 @@
 #include "ColliderManager.h"
 #include "GameObject.h"
 #include "input.h"
+#include "PhysicsComponent.h"
 
 class Coin : public GameObject
 {
 private:
-
+	PhysicsComponent* phy_;
 public:
 	Coin(Input& input, ColliderManager& cm, const Vec2<float>& position);
 	void Update(const float& frametime) override;

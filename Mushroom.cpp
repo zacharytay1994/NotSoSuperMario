2e2579b68466b49909188f7a6bf055fda22fc92a
= <sim.sheline@gmail.com>
2019-12-04 14:14:18 +0800

    moving mushroom!

--- Mushroom.cpp
+++ Mushroom.cpp
@@ -1,4 +1,5 @@
 #include "Mushroom.h"
+#include <Windows.h>
 
 Mushroom::Mushroom(ColliderManager& cm, const Vec2<float>& position, Mario& m)
 	:
@@ -7,32 +8,46 @@ Mushroom::Mushroom(ColliderManager& cm, const Vec2<float>& position, Mario& m)
 	mario_(&m)
 {
 	// Initialize PhysicsComponent & add it to the Mushroom
-	phy_ = new PhysicsComponent(*this, 0, false);
+	phy_ = new PhysicsComponent(*this);
 	AddComponent(phy_);
 
 	// Initialize CollisionDetectionComponent & add it to the Mushroom
-	AddComponent(new CollisionDetectionComponent<AABBCollider>(*this, new AABBCollider(position_, this, sprite_->GetWidth() - 1, sprite_->GetHeight() - 6, false, false), cm));
+	AddComponent(new CollisionDetectionComponent<AABBCollider>(*this, new AABBCollider(position_, this, sprite_->GetWidth() - 4, sprite_->GetHeight(), false, true), cm));
+
+	// Calculate direction
+	direction_ = mario_->GetPosition().x > position_.x ? 1.0f : -1.0f;
 }
 
 void Mushroom::Update(const float& frametime)
 {
 	GameObject::Update(frametime);
+	
+	// Move
+	phy_->AddVelocity(Vec2<float>(speed_ * direction_, 0.0f));
 
-	// TODO: if collided with, mario goes Super
-	if (touch_.touch_top_ || touch_.touch_bottom_ || touch_.touch_left_ || touch_.touch_right_)
+	// Mario x Mushroom collision: Mario goes Super
+	if (touch_.touch_top_ && touch_obj_.touch_obj_top_->owner_->type_ == "Mario" || 
+		touch_.touch_bottom_ && touch_obj_.touch_obj_bottom_->owner_->type_ == "Mario" || 
+		touch_.touch_left_ && touch_obj_.touch_obj_left_->owner_->type_ == "Mario" || 
+		touch_.touch_right_ && touch_obj_.touch_obj_right_->owner_->type_ == "Mario")
 	{
-		removed_ = true;
+			removed_ = true;
 
-		// mario goes super
-		mario_->ChangeSprite(mario_->super_idle_);
-		mario_->hold_ = mario_->super_idle_;
-		mario_->running_animation_ = mario_->super_running_animation_;
-		mario_->jumping_ = mario_->super_jumping_;
+			// mario goes super
+			mario_->ChangeSprite(mario_->super_idle_);
+			mario_->hold_ = mario_->super_idle_;
+			mario_->running_animation_ = mario_->super_running_animation_;
+			mario_->jumping_ = mario_->super_jumping_;
 
-		CollisionDetectionComponent<AABBCollider>* c = dynamic_cast<CollisionDetectionComponent<AABBCollider>*>(mario_->GetComponent("CollisionDetectionComponent"));
-		c->SetColliderHeight(128 * CAMERA_ZOOM);
+			CollisionDetectionComponent<AABBCollider>* c = dynamic_cast<CollisionDetectionComponent<AABBCollider>*>(mario_->GetComponent("CollisionDetectionComponent"));
+			c->SetColliderHeight(128 * CAMERA_ZOOM);
 
-		mario_->is_big_ = true;
+			mario_->is_big_ = true;
+	}
+	else if (touch_.touch_left_ && touch_obj_.touch_obj_left_->owner_->type_ != "Mario" || 
+			 touch_.touch_right_ && touch_obj_.touch_obj_right_->owner_->type_ != "Mario")
+	{
+		direction_ *= -1.0f;
 	}
 }
 
